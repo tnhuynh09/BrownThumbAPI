1da9c04682fc635c4fb3c23252f71b69b1dc4826
Emma Huynh <emmahuynh@Emmas-MBP.fios-router.home>
2020-08-20 01:43:35 -0700

    add models

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
+import os 
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+from sqlalchemy.exc import IntegrityError
+
+from models import db, connect_db, User, Plant, ProgressJournal, UserPlant, PlantJournal
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "br0wnthumbs"
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgres:///brown_thumb'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "br0wnthumbs")
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+
 
 @app.route("/test", methods=["GET"])
 def test():
+    return ("WORKS")
+
+@app.route("/users/signup", methods=["GET", "POST"])
+def signup():
+    """Handle user signup."""
+
+    
+
     return ("WORKS")
---
+++ models.py
@@ -0,0 +1,227 @@
+"""SQLAlchemy models for Brown Thumb."""
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+from datetime import datetime
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+class User(db.Model):
+    """User in the system."""
+
+    __tablename__ = 'users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    username = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    password = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    image_url = db.Column(
+        db.Text,
+        default="/static/images/default-pic.png",
+    )
+
+    plants_owned = db.relationship('UserPlant', backref='user')
+
+    def __repr__(self):
+        return f"<User #{self.id}: {self.username}>"
+
+    @classmethod
+    def signup(cls, username, password, image_url):
+        """Sign up user.
+        Hashes password and adds user to system.
+        """
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = User(
+            username=username,
+            password=hashed_pwd,
+            image_url=image_url,
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Find user with `username` and `password`.
+
+        This is a class method (call it on the class, not an individual user.)
+        It searches for a user whose password hash matches this password
+        and, if it finds such a user, returns that user object.
+
+        If can't find matching user (or if password is wrong), returns False.
+        """
+
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
+
+class Plant(db.Model):
+    """Plants saved from an external database."""
+
+    __tablename__ = 'plants'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    plant_api_id = db.Column(
+        db.Integer,
+        nullable=False,
+    )
+
+    common_name = db.Column(
+        db.Text,
+    )
+
+    scientific_name = db.Column(
+        db.Text,
+    )
+
+    family = db.Column(
+        db.Text,
+    )
+
+    family_common_name = db.Column(
+        db.Text,
+    )
+
+    genus = db.Column(
+        db.Text,
+    )
+
+    image_url = db.Column(
+        db.Text,
+        default="/static/images/default-pic.png",
+    )
+
+    plants_owned = db.relationship('UserPlant', backref='plants')
+
+    journal_entry = db.relationship('PlantJournal', backref='plants')
+
+class ProgressJournal(db.Model):
+    """Journal for each plants that the user add to their profile."""
+
+    __tablename__ = 'progress_journals'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    title = db.Column(
+        db.String(100),
+        nullable=False,
+    )
+
+    date = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow(),
+    )
+
+    image_url = db.Column(
+        db.Text,
+        default="/static/images/default-pic.png",
+    )
+
+    notes = db.Column(
+        db.String(300),
+        nullable=False,
+    )
+
+    journal_entry = db.relationship('PlantJournal', backref='progress_journals')
+
+class UserPlant(db.Model):
+    """References the plants that each user added."""
+
+    __tablename__ = 'users_plants'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+
+    plant_id = db.Column(
+        db.Integer,
+        db.ForeignKey('plants.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+
+    date_purchased = db.Column(
+        db.DateTime,
+        default=datetime.utcnow(),
+    )
+
+    light_amt_req = db.Column(
+        db.String(100),
+    )
+
+    water_amt_req = db.Column(
+        db.String(100),
+    )
+
+    common_pests = db.Column(
+        db.String(100),
+    )
+
+    notes = db.Column(
+        db.String(100),
+    )
+
+class PlantJournal(db.Model):
+    """Reference the journals added to each plants."""
+
+    __tablename__ = 'plants_journals'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    plant_id = db.Column(
+        db.Integer,
+        db.ForeignKey('plants.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+
+    journal_id = db.Column(
+        db.Integer,
+        db.ForeignKey('progress_journals.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
